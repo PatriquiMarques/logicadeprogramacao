c3b6a159e260c892fe03bd1bb8a41e948840ae9d
PatriquiMarques <patriqui.marques@hotmail.com>
2024-06-23 09:45:24 -0300

    Resolução

--- estrutura de repeticao/src/exercicio04_2006.js
+++ estrutura de repeticao/src/exercicio04_2006.js
@@ -4,21 +4,24 @@ import leia from "readline-sync"
 var cont = 1
 
 
+var num = leia.questionFloat("Digite um numero\n")
+var maior = num
+var menor = num
 
 
-
-while( cont<= 15){
+while( cont<= 3){
     cont++
    
-    var num = leia.questionFloat("Digite um numero\n")
- 
+    var num2 = leia.questionFloat("Digite um numero\n")
     
-    if (num > 0 ){
+    
+    if (num2 > num && num2 > maior  ){
          
-       var  maior= num
-    }else(
-       var menor = num
-    )   
+         maior= num2
+    }else{
+       menor = num2
+    } 
+
 
 
     } console.log(" O maior numero digitado é: " +  maior + "\n" + "O numero menor é: " + menor)
