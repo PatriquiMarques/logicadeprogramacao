ef8a5fd5be9c2f523c107d02a1dc3464e5caaace
PatriquiMarques <patriqui.marques@hotmail.com>
2024-06-27 21:52:30 -0300

    exemplo de vetor

---
+++ estruturas de dados/src/exemploVetor.js
@@ -0,0 +1,15 @@
+// import leia from "readline-sync"
+
+// var num = leia.questionFloat("Digite um numero\n")
+// var zero = 0
+
+// while ( num >= zero ){
+
+//     console.log(num)
+//     num--
+  
+// }
+
+var lista = ["Patriqui", "Marques"]
+
+console.log(lista[0])
